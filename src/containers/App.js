69c55ab9971491300000b20bf68861c340eb327f
Asif <mdasif.hossain1996@gmail.com>
2023-01-25 19:28:59 +0600

    Functional Conversion

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,47 +1,74 @@
-import React, { Component } from "react";
+import React, { useEffect, useState } from "react";
 import CardList from "../components/CardList";
 import SearchBox from "../components/SearchBox";
 import Scroll from "../components/Scroll";
 import "./App.css";
 
-class App extends Component {
-  constructor() {
-    super();
-    this.state = {
-      robots: [],
-      searchfield: "",
-    };
-  }
+export default function App() {
+  const [robots, setRobot] = useState([]);
+  const [searchfield, setSearchfield] = useState("");
 
-  componentDidMount() {
+  useEffect(() => {
     fetch("https://jsonplaceholder.typicode.com/users")
       .then((response) => response.json())
       .then((users) => {
-        this.setState({ robots: users });
+        setRobot(users);
       });
-  }
+  }, []);
 
-  onSearchChange = (event) => {
-    this.setState({ searchfield: event.target.value });
-  };
+  const filteredRobots = robots.filter((robot) => {
+    return robot.name.toLowerCase().includes(searchfield.toLowerCase());
+  });
 
-  render() {
-    const { robots, searchfield } = this.state;
-    const filteredRobots = robots.filter((robot) => {
-      return robot.name.toLowerCase().includes(searchfield.toLowerCase());
-    });
-    return !robots.length ? (
-      <h1>Loading</h1>
-    ) : (
-      <div className="tc">
-        <h1 className="f1">Robo Friends</h1>
-        <SearchBox searchChange={this.onSearchChange} />
-        <Scroll>
-          <CardList robots={filteredRobots} />
-        </Scroll>
-      </div>
-    );
-  }
+  return !robots.length ? (
+    <h1>Loading</h1>
+  ) : (
+    <div className="tc">
+      <h1 className="f1">Robo Friends</h1>
+      <SearchBox setSearchfield={setSearchfield} />
+      <Scroll>
+        <CardList robots={filteredRobots} />
+      </Scroll>
+    </div>
+  );
 }
 
-export default App;
+// class App extends Component {
+//   constructor() {
+//     super();
+//     this.state = {
+//       robots: [],
+//       searchfield: "",
+//     };
+//   }
+
+//   componentDidMount() {
+//     fetch("https://jsonplaceholder.typicode.com/users")
+//       .then((response) => response.json())
+//       .then((users) => {
+//         this.setState({ robots: users });
+//       });
+//   }
+
+//   onSearchChange = (event) => {
+//     this.setState({ searchfield: event.target.value });
+//   };
+
+//   render() {
+//     const { robots, searchfield } = this.state;
+//     const filteredRobots = robots.filter((robot) => {
+//       return robot.name.toLowerCase().includes(searchfield.toLowerCase());
+//     });
+//     return !robots.length ? (
+//       <h1>Loading</h1>
+//     ) : (
+//       <div className="tc">
+//         <h1 className="f1">Robo Friends</h1>
+//         <SearchBox searchChange={this.onSearchChange} />
+//         <Scroll>
+//           <CardList robots={filteredRobots} />
+//         </Scroll>
+//       </div>
+//     );
+//   }
+// }
